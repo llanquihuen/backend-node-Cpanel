d4226deb3190c6fdf178938f70a5348591bbfb93
Luis Ramirez <llanquihuen@gmail.com>
2021-06-13 23:56:15 -0400

    envPass

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ dotenv.config()
 const pool = mysql.createPool({
     host: 'localhost',
     user: process.env.DB_USER,
-    password: '',
+    password: process.env.DB_PASS,
     database: process.env.DB_DB,
     connectionLimit: 10,
 });
